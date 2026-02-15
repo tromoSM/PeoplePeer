9302e1a84a7f519e602d1bec4930b2e4d1ab67f7
tromoSM <tromosm7@gmail.com>
2026-02-16 03:58:30 +0530

    Update user.js

--- js/user.js
+++ js/user.js
@@ -1,8 +1,1 @@
-window.addEventListener("DOMContentLoaded",async function(){
- const {data}=await supabase.auth.getSession()
- if(data.session){
-    document.querySelectorAll(`sign`).forEach(dih=>{
-        dih.remove()
-    })
- }
-})
+
