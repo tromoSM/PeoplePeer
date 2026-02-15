6ffc98e3fe1a133bd89fa93014d473b2d181face
tromoSM <tromosm7@gmail.com>
2026-02-15 13:02:50 +0530

    Update identity.js

--- js/identity.js
+++ js/identity.js
@@ -4,8 +4,58 @@ window.supabase=supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
  )
+ 
 window.loginG=async function(){
     await supabase.auth.signInWithOAuth({
     provider:"google"
     })
+}
+async function signE(){
+    const{data,error}=await supabase.auth.signInWithPassword({
+        email: document.querySelector(`[val="mailin"]`).value.trim(),
+        password:document.querySelector(`[val="passin"]`).value.trim()
+    
+    }   
+)
+    if(error){
+        console.error(error.message)
+        return
+    }
+}
+async function signUP(){
+    const{data,error}=await supabase.auth.signUp({
+        email: document.querySelector(`[val="mailup"]`).value.trim(),
+        password:document.querySelector(`[val="passup"]`).value.trim()
+    
+    })
+    if(error){
+        console.error(error.message)
+        return
+    }
+    else{
+        if(data.session){
+            document.querySelector('[si="sign"]').style.display="none"
+            document.querySelector('[si="sign"]').style.display="flex"
+        }
+    }
+}
+
+/* 
+
+    if (data.session) {
+        document.querySelector('[si="sign"]').style.display = "none"
+        document.querySelector('[si="infoin"]').style.display = "flex"
+    }
+}
+*/
+async function getin(){
+    const user=data.user
+    await supabase.from("profiles").insert([
+    {
+            id: user.id,
+            username: document.querySelector('[val="usrname"]').value.trim(),
+            birthday: document.querySelector('[val="date"]').value,
+            avatar_url:null
+    }
+    ])
 }
